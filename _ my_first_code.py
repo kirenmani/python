7632cf7d09848b45c35a93918c2473362b318a3b
kirenmani <133827435+kirenmani@users.noreply.github.com>
2023-05-24 11:12:12 +1000

    Add files via upload

---
+++ _ my_first_code.py
@@ -0,0 +1,11 @@
+
+# Online IDE - Code Editor, Compiler, Interpreter
+print("Kiren")
+print(2*"\n")
+print("Its a Monday")
+print(4*"\n")
+print('Julian Date is ?')
+print(6*"\n")
+print("this is my created code")
+
+
