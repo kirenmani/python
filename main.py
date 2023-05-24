fb11d2d1b8e6f9aa528643f776f5e94d785515ea
kirenmani <133827435+kirenmani@users.noreply.github.com>
2023-05-24 11:01:29 +1000

    Add files via upload
    commit 1
    co

---
+++ main.py
@@ -0,0 +1,15 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+a = "Hello, this is my first"
+b= " ,time using variable."
+print(a+b)
+c = "hello john is my first do time"
+d =  "i am on google meet"
+print(c+d)
+f = "It is lovely sunny day"
+e = "What time in india"
+print(f+e)
+
+
+
+
